013860e755ba7ecfd5ee264de95c949b9eeb5590
Murchi228pro <102650348+Murchi228pro@users.noreply.github.com>
2023-07-17 10:28:35 +0300

    Update main.py
    Теперь при поедании квадратов персонаж будет на слой выше

--- main.py
+++ main.py
@@ -92,7 +92,6 @@ while True:
 
         window.fill(BG_COLOR)
 
-        pygame.draw.rect(window, PERSON_COLOR, person, 10)
         foods = [pygame.draw.rect(window, random.choice(RANDOM_COLORS),
                                   (*i, 5, 5))for i in food_list]
         for food in enumerate(foods, 0):
@@ -112,6 +111,7 @@ while True:
         )
         window.blit(score, (10, 10))
         window.blit(before_death_text, (650, 10))
+        pygame.draw.rect(window, PERSON_COLOR, person, 10)
 
         pygame.display.update()
 
